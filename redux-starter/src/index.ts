aa4a601ef18f333f0fb32b1ae0b8968f70293b69
Tom Hughes <thomas.hughes@sageappliances.com>
2024-03-02 13:42:24 +0000

    trying lodash and func programming

--- redux-starter/src/index.ts
+++ redux-starter/src/index.ts
@@ -1,1 +1,23 @@
+import {compose, pipe} from "lodash/fp"
+
+
 console.log("The Redux Starter Project!!");
+
+
+
+let user = "   Tom  "
+
+const trim = (name:string) => name.trim()
+const message = (trimmedName:string) => `Hi there ${trimmedName}!`
+const pipeMessage = (mainMessage:string) => `${mainMessage} Generated with pipe lodash`
+const printMessage = (fullMessage:string)=> console.log(fullMessage)
+
+const newComposeFuncLodash: (name: string) => void = compose(printMessage,message,trim)
+newComposeFuncLodash(user)
+
+
+
+
+const newPipeFuncLodash: (name: string) => void = pipe(trim,message,pipeMessage,printMessage)
+newPipeFuncLodash(user)
+// console.log(newPipeFuncLodash);
