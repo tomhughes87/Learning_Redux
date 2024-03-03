c8fc66a6cf0aa0e6a85e7ffcaac646d7b72cc9cc
Tom Hughes <thomas.hughes@sageappliances.com>
2024-03-03 15:52:24 +0000

    add devToolEnhancer-broken

--- redux-starter/src/store/store.ts
+++ redux-starter/src/store/store.ts
@@ -1,9 +1,10 @@
 import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import reducer from "./Tasks/reducer";
 import { thunk } from "redux-thunk";
-import { composeWithDevTools } from "@redux-devtools/extension";
+import { devToolsEnhancer } from "@redux-devtools/extension";
 
 export const store = createStore(
   reducer,
-  composeWithDevTools(applyMiddleware(thunk))
+  devToolsEnhancer({ trace: true }),
+  applyMiddleware(thunk)
 );
