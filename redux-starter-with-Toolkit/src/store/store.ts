d13d5a59da8df9551a893516af11989fd3283577
Tom Hughes <thomas.hughes@sageappliances.com>
2024-03-03 16:45:46 +0000

    setup toolkit store

--- redux-starter-with-Toolkit/src/store/store.ts
+++ redux-starter-with-Toolkit/src/store/store.ts
@@ -1,10 +1,4 @@
-import { legacy_createStore as createStore, applyMiddleware } from "redux";
 import reducer from "./Tasks/reducer";
-import { thunk } from "redux-thunk";
-import { devToolsEnhancer } from "@redux-devtools/extension";
+import { configureStore } from "@reduxjs/toolkit";
 
-export const store = createStore(
-  reducer,
-  devToolsEnhancer({ trace: true })
-  //   applyMiddleware(thunk)
-);
+export const store = configureStore({ reducer: reducer });
